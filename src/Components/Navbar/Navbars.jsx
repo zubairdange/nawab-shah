a799673db7d4e6c86c229eadb4de10090e1298fa
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-04 18:06:42 +0530

    added nav bar

--- src/Components/Navbar/Navbars.jsx
+++ src/Components/Navbar/Navbars.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { Navbar, Nav, FormControl, Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 export const Navbars = () => {
   return (
     <div>
-      <h1>hiiiiiii</h1>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+      <br />
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form>
+      </Navbar>
     </div>
   );
 };
