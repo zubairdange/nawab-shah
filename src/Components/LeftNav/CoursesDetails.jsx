3fae980629a9be3b099093627c06277d40225d14
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-06 14:35:56 +0530

    ADDED OTHER COURSES

--- src/Components/LeftNav/CoursesDetails.jsx
+++ src/Components/LeftNav/CoursesDetails.jsx
@@ -86,6 +86,92 @@ export const CoursesDetails = () => {
             <Nav.Link style={{ fontSize: "16px" }}>PEO's & PSO's</Nav.Link>
 
             <Nav.Link style={{ fontSize: "16px" }}>Course OutCome</Nav.Link>
+            <Button
+              style={{
+                marginTop: "10px",
+                marginLeft: "10px",
+                marginRight: "10px",
+                fontSize: "20px",
+              }}
+            >
+              BE (IOT,CYBER SECURITY,BLOCK CHAIN)
+            </Button>
+            <Nav.Link style={{ fontSize: "16px" }}>About Course</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>Vision & Mision</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>PEO's & PSO's</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>Course OutCome</Nav.Link>
+            <Button
+              style={{
+                marginTop: "10px",
+                marginLeft: "10px",
+                marginRight: "10px",
+                fontSize: "20px",
+              }}
+            >
+              BE (CIVIL)
+            </Button>
+            <Nav.Link style={{ fontSize: "16px" }}>About Course</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>Vision & Mision</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>PEO's & PSO's</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>Course OutCome</Nav.Link>
+            <Button
+              style={{
+                marginTop: "10px",
+                marginLeft: "10px",
+                marginRight: "10px",
+                fontSize: "20px",
+              }}
+            >
+              BE (MECH)
+            </Button>
+            <Nav.Link style={{ fontSize: "16px" }}>About Course</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>Vision & Mision</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>PEO's & PSO's</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>Course OutCome</Nav.Link>
+
+            <Button
+              style={{
+                marginTop: "10px",
+                marginLeft: "10px",
+                marginRight: "10px",
+                fontSize: "20px",
+              }}
+            >
+              BE (ECE)
+            </Button>
+            <Nav.Link style={{ fontSize: "16px" }}>About Course</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>Vision & Mision</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>PEO's & PSO's</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>Course OutCome</Nav.Link>
+            <Button
+              style={{
+                marginTop: "10px",
+                marginLeft: "10px",
+                marginRight: "10px",
+                fontSize: "20px",
+              }}
+            >
+              BE (EEE)
+            </Button>
+            <Nav.Link style={{ fontSize: "16px" }}>About Course</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>Vision & Mision</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>PEO's & PSO's</Nav.Link>
+
+            <Nav.Link style={{ fontSize: "16px" }}>Course OutCome</Nav.Link>
           </Card>
         </Col>
         <Col lg="9">{/* <h2>hiiiiii</h2> */}</Col>
