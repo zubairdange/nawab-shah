d4d02aa7196db6657fbea50757ae107e55d5d87f
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-06 18:10:58 +0530

    deleted comment

--- src/Components/LeftNav/Home.jsx
+++ src/Components/LeftNav/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { UGCoursesDetails } from "./UGCoursesDetails";
-import { PGCoursesDetails } from "./PGCoursesDetails";
-import { Row, Col, Card } from "react-bootstrap";
+
 export const Home = () => {
   return (
     <div>
