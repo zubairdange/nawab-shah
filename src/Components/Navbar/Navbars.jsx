a69c513ed8e45a35cb4c746caad0deaff357c3ea
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-06 13:26:45 +0530

    changes thr href

--- src/Components/Navbar/Navbars.jsx
+++ src/Components/Navbar/Navbars.jsx
@@ -5,16 +5,16 @@ export const Navbars = () => {
   return (
     <div>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
+        {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
+        <Nav>
           <Nav.Link href="#home">HOME</Nav.Link>
-          <Nav.Link href="#features">ABOUT US </Nav.Link>
-          <Nav.Link href="#pricing">COURSES</Nav.Link>
-          <Nav.Link href="#pricing">DEPARTMENT</Nav.Link>
-          <Nav.Link href="#pricing">ADMISSIONS</Nav.Link>
-          <Nav.Link href="#pricing">PLACEMENTS</Nav.Link>
-          <Nav.Link href="#pricing">FEATURES</Nav.Link>
-          <Nav.Link href="#pricing">EVENTS</Nav.Link>
+          <Nav.Link href="#aboutus">ABOUT US </Nav.Link>
+          <Nav.Link href="#courses">COURSES</Nav.Link>
+          <Nav.Link href="#department">DEPARTMENT</Nav.Link>
+          <Nav.Link href="#admissions">ADMISSIONS</Nav.Link>
+          <Nav.Link href="#placement">PLACEMENTS</Nav.Link>
+          <Nav.Link href="#features">FEATURES</Nav.Link>
+          <Nav.Link href="#events">EVENTS</Nav.Link>
           <Nav.Link href="#pricing">GALLERY</Nav.Link>
           <Nav.Link href="#pricing">EXAMINATION CELL</Nav.Link>
           <Nav.Link href="#pricing">R & D CELL</Nav.Link>
