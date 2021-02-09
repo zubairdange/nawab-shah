b452ea1bb9462a14db6a4f78a6fa3bb835fbaab5
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-09 12:10:19 +0530

    added component Source outcome it

--- src/Components/LeftNav/UGCourses/BE(IT)/SourceOutcomeIT.jsx
+++ src/Components/LeftNav/UGCourses/BE(IT)/SourceOutcomeIT.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export const SourceOutcomeIT = () => {
   return (
     <div>
-      <h3 className="headerclass">CSE Course Outcomes</h3>
+      <h3 className="headerclass">IT Course Outcomes</h3>
     </div>
   );
 };
