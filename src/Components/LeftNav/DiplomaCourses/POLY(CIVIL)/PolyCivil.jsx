d00596cb46fd94ed41a7bd6da596f3f0e959e454
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-10 12:29:09 +0530

    added content in poly civil and mech course

--- src/Components/LeftNav/DiplomaCourses/POLY(CIVIL)/PolyCivil.jsx
+++ src/Components/LeftNav/DiplomaCourses/POLY(CIVIL)/PolyCivil.jsx
@@ -1,8 +1,87 @@
 import React from "react";
+import imgLogo from "../../../Images/PolyCivil.png";
 export const PolyCivil = () => {
   return (
     <div>
-      <h1>poly civil</h1>
+      <h3 className="headerclass">CIVIL Engineering (POLY)</h3>
+      <img src={imgLogo} className="cseimage"></img>
+      <h5>Diploma In Civil Engineering.</h5>
+      <p>
+        The college offers Polytechnic (Civil Engineering) course whose details
+        are given below:
+      </p>
+      <h4>AN INTRODUCTION TO CIVIL ENGINEERING</h4>
+      <p>
+        If you have fondness in building new structures and wish to always have
+        your desired job in hand, civil engineering is your best choice. Civil
+        engineering is one of the most traditional engineering studies in India.
+        British started very good schools for civil engineering studies and
+        successfully completed some of the best public projects like Ganga canal
+        and India’s Railway network to name few. A diploma in civil engineering
+        will give good opportunities in industry as India is moving fast forward
+        for infrastructure development and one the same time leave a good space
+        for higher studies at home and abroad like B.Tech. in Civil Engineering.
+      </p>
+      <p>
+        The primary objective of the Diploma Program of the Civil Engineering
+        Technology curriculum is to introduce students to the employment
+        opportunities in civil engineering technology. The objective is
+        fulfilled through the study and application of civil engineering
+        technology courses. Students will have the advantages of studying in a
+        multi-faculty college with a long tradition of excellence in teaching,
+        research and scholarship. There will be an emphasis on developing skills
+        within a teamwork environment, equipping the students for subsequent
+        professional practice.
+      </p>
+      <p>
+        Our College offers the course work of VI semester for Diploma in Civil
+        Engineering.
+      </p>
+      <h4>OPPURTUNITIES & PROSPECTS AFTER COMPLETION</h4>
+      <ul>
+        <li>
+          The country is developing fast and it needs infrastructures to sustain
+          this development, which require qualified civil engineers to build
+          them. You can find jobs in construction companies, private consulting
+          firms, government departments, research institutes etc.
+        </li>
+        <li>
+          Diploma Civil Engineer is generally employed as a Site Engineer, a
+          Surveyor, Supervisor of various processes, an assistant to
+          Designer/Consultant, Valuer, as a planner etc.
+        </li>
+      </ul>
+      <h5>The career options for Diploma Civil Engineer are</h5>
+      <ol style={{ textAlign: "left" }}>
+        <li>Buildings / Civil works Site Supervisor</li>
+        <li>Transportation system work Engineer</li>
+        <li>Maintenance Engineer </li>
+        <li>Material Supplier / Manufacturer</li>
+        <li>Building Services</li>
+        <li>Architectural Assistance / Practice Consulting Engineer</li>
+        <li>Contractor / Builder</li>
+      </ol>
+      <p>
+        The civil engineers working in industry are often responsible for
+        construction planning, preparation and supervision. Planning task
+        involves drawing work, cost calculation site matters. In the actual
+        construction stage the civil engineer functions as a site supervisor.
+        Additional responsibilities include organize the deployment of
+        resources.
+      </p>
+      <h5>Top Employers in India</h5>
+      <ul>
+        <li>Larsen &amp; Toubro</li>
+        <li>DLF</li>
+        <li>Tata Projects</li>
+        <li>Gammon India</li>
+        <li>Hindustan Construction Company</li>
+        <li>Sobha Developers Ltd</li>
+        <li>Shapoorji Pallonji &amp; Co</li>
+        <li>Unitech</li>
+        <li>Nagarjuna Construction Company</li>
+        <li>Punj Lloyd</li>
+      </ul>
     </div>
   );
 };
--- src/Components/LeftNav/DiplomaCourses/POLY(ME)/PolyMech.jsx
+++ src/Components/LeftNav/DiplomaCourses/POLY(ME)/PolyMech.jsx
@@ -1,8 +1,93 @@
 import React from "react";
+import imgLogo from "../../../Images/PolyMech.png";
+
 export const PolyMech = () => {
   return (
     <div>
-      <h1>poly Mech</h1>
+      <h3 className="headerclass">MECHANICAL Engineering (POLY)</h3>
+      <img src={imgLogo} className="cseimage"></img>
+      <h5>Diploma In Mechanical Engineering.</h5>
+      <p>
+        The college offers Polytechnic (Mechanical Engineering) course whose
+        details are given below:
+      </p>
+      <h4>AN INTRODUCTION TO MECHANICAL ENGINEERING</h4>
+      <p>
+        A study in mechanical engineering, hence, will equip you with this basic
+        knowledge of movement and the power / energy that is involved, which
+        makes it possible. The scope of study and the flexibility of career
+        choice enjoyed by this field of engineering is unmatched by any other
+        field.
+      </p>
+      <p>
+        Mechanical engineering study is applied in the design and control of
+        systems and processes involved in – manufacturing plants, automobiles,
+        aircrafts, machine tools, air-conditioning, refrigeration, boilers,
+        power plants, fuel processing etc.
+      </p>
+      <p>
+        With the Diploma in Mechanical Engineering (ME), you can look forward to
+        creating and designing products for diverse sectors. You will gain a
+        strong foundation and become a competent mechanical engineer. This
+        practice oriented course will provide you with the latest computational
+        skills and technology to be at the forefront of the industry. ME’s
+        strong emphasis on design empowers you for value-added engineering jobs
+        such as the generation and use of energy, creation of new materials,
+        process design, and the manufacture of a wide array of products ranging
+        from home appliances to medical devices.
+      </p>
+      <p>
+        Our College offers the course work of VI semester for Diploma in
+        Mechanical Engineering.
+      </p>
+
+      <h4>OPPURTUNITIES & PROSPECTS AFTER COMPLETION</h4>
+      <ul>
+        <li>
+          In general, mechanical engineers are involved in the working related
+          to motion of a particle or machinery and energy transfer from one
+          position to other in a system. The job varies from the field of
+          specialisation and the career path you have chosen.
+        </li>
+        <li>Basically, a mechanical engineer’s work involves –</li>
+      </ul>
+      <ol style={{ textAlign: "left" }}>
+        <li>
+          Design and control of system that goes into the process of
+          manufacturing the machinery and product.
+        </li>
+        <li>Testing of new systems.</li>
+        <li>Quality management and improvement.</li>
+        <li>Marketing products and systems.</li>
+      </ol>
+      <ul>
+        <li>
+          If one enquires a field of study that had guaranteed equal demand in
+          the past and in the coming future – its mechanical engineering.
+        </li>
+        <li>
+          With its wide scope of application, mechanical engineers remain always
+          in demand, no matter which part of the world.
+        </li>
+      </ul>
+      <h5>You can find job in the following industries:-</h5>
+      <ol style={{ textAlign: "left" }}>
+        <li>Manufacturing</li>
+        <li>Automobile</li>
+        <li>Power</li>
+        <li>Aeronautical</li>
+        <li>Biotechnology</li>
+      </ol>
+      <h5>Top Employers in India:</h5>
+      <ul>
+        <li>JINDAL</li>
+        <li>Maruti</li>
+        <li>Hind Motor</li>
+        <li>TATA</li>
+        <li>SAIL</li>
+        <li>BHEL(recruit some mechanical engineering student)</li>
+        <li>Indian Railways etc.</li>
+      </ul>
     </div>
   );
 };
