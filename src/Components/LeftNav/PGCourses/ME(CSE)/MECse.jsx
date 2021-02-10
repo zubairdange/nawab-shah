3b62110800a494d95aa90d48fb5e512d908b08cc
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-10 10:47:32 +0530

    ADDED SOME CONTENTS IN meCSE COMPONENT

--- src/Components/LeftNav/PGCourses/ME(CSE)/MECse.jsx
+++ src/Components/LeftNav/PGCourses/ME(CSE)/MECse.jsx
@@ -29,6 +29,58 @@ export const MECse = () => {
         the student. Most common co-curricular activity is the programming
         contest, which has become regular by this time.
       </p>
+      <h4>ABOUT PROGRAM</h4>
+      <ul>
+        <li>
+          Our college offers a four-semester M. Tech course in Computer Science
+          &amp; Engineering as per JNTUH syllabus with first two semesters
+          consisting of course work, third semester consisting of a
+          comprehensive viva-voce examination and the last semester consisting
+          of project work.
+        </li>
+      </ul>
+      <h4>SYLLABUS</h4>
+      <ul>
+        <li>M. TECH. COMPUTER SCIENCE ENGINEERING-R13 Regulations</li>
+        <li>
+          You can download from here{" "}
+          <a href="images/pdf/COMPUTER_SCIENCE_ENGINEERING.pdf" target="_blank">
+            COMPUTER_SCIENCE_ENGINEERING.pdf
+          </a>
+        </li>
+      </ul>
+      <h4>OBJECTIVES</h4>
+      <ul>
+        <li>
+          Computer Engineers can work as a Software/Hardware Engineer, which are
+          the two main sectors in the computer industry.
+        </li>
+        <li>
+          They can work as programmers, web developers and E-commerce
+          specialists with industries that build or use Computer-based systems,
+          such as telecommunications, automotive, aerospace, etc.
+        </li>
+        <li>To embrace constant changes in technology and curriculum.</li>
+        <li>
+          National and multinational computer manufacturing companies, computer
+          networking companies, Software development companies as well as
+          hardware assembling and maintenance companies, data processing
+          departments of modern industries and in areas where computers and
+          computer-aided systems are used, such as Aeronautics and space
+          science, the petroleum industry, chemical plants and power plants and
+          management Consultancy organizations are some among the probable work
+          places.
+        </li>
+        <li>
+          There is a great demand for professionals involved in the Testing and
+          Designing of computer components.
+        </li>
+        <li>
+          Employment of Computer Software Engineers is expected to increase by
+          32 percent from 2008-2018, which is much faster than the average for
+          all occupations.
+        </li>
+      </ul>
     </div>
   );
 };
