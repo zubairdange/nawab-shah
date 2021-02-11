02f729afb3955ebf0227fc8201b48a0794cab201
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-11 18:18:38 +0530

    added some data

--- src/Components/LeftNav/UGCourses/BE(IT)/SourceOutcomeIT.jsx
+++ src/Components/LeftNav/UGCourses/BE(IT)/SourceOutcomeIT.jsx
@@ -5,32 +5,72 @@ export const SourceOutcomeIT = () => {
     <div>
       <h3 className="headerclass">IT Course Outcomes</h3>
       <h5 className="h5class">IT-R09</h5>
-      <Button className="buttonclass">BTech(IT) R09 1</Button>
-      <Button className="buttonclass">BTech(IT) R03 2-1</Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R09-1.pdf">
+        BTech(IT) R09 1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R09-2-1.pdf">
+        BTech(IT) R03 2-1
+      </Button>
 
-      <Button className="buttonclass">BTech(IT) R09 2-2</Button>
-      <Button className="buttonclass">BTech(IT) R09 3-1</Button>
-      <Button className="buttonclass">BTech(IT) R09 3-2</Button>
-      <Button className="buttonclass">BTech(IT) R09 4-1</Button>
-      <Button className="buttonclass">BTech(IT) R09 4-2</Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R09-2-2.pdf">
+        BTech(IT) R09 2-2
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R09-3-1.pdf">
+        BTech(IT) R09 3-1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R09-3-2.pdf">
+        BTech(IT) R09 3-2
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R09-4-1.pdf">
+        BTech(IT) R09 4-1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R09-4-2.pdf">
+        BTech(IT) R09 4-2
+      </Button>
       <h5 className="h5class">IT-R13</h5>
-      <Button className="buttonclass">BTech(IT) R13 1</Button>
-      <Button className="buttonclass">BTech(IT) R13 2-1</Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R13-1.pdf">
+        BTech(IT) R13 1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R13-2-1.pdf">
+        BTech(IT) R13 2-1
+      </Button>
 
-      <Button className="buttonclass">BTech(IT) R13 2-2</Button>
-      <Button className="buttonclass">BTech(IT) R13 3-1</Button>
-      <Button className="buttonclass">BTech(IT) R13 3-2</Button>
-      <Button className="buttonclass">BTech(IT) R13 4-1</Button>
-      <Button className="buttonclass">BTech(IT) R13 4-2</Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R13-2-1.pdf">
+        BTech(IT) R13 2-2
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R13-3-1.pdf">
+        BTech(IT) R13 3-1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R13-3-2.pdf">
+        BTech(IT) R13 3-2
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R13-4-1.pdf">
+        BTech(IT) R13 4-1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R13-4-2.pdf">
+        BTech(IT) R13 4-2
+      </Button>
       <h5 className="h5class">IT-R16</h5>
-      <Button className="buttonclass">BTech(IT) R16 1-1</Button>
-      <Button className="buttonclass">BTech(IT) R16 1-2</Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R16-1-1.pdf">
+        BTech(IT) R16 1-1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R16-1-2.pdf">
+        BTech(IT) R16 1-2
+      </Button>
 
-      <Button className="buttonclass">BTech(IT) R16 2-1</Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R16-2-1.pdf">
+        BTech(IT) R16 2-1
+      </Button>
 
-      <Button className="buttonclass">BTech(IT) R16 2-2</Button>
-      <Button className="buttonclass">BTech(IT) R16 3-1</Button>
-      <Button className="buttonclass">BTech(IT) R16 3-2</Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R16-2-2.pdf">
+        BTech(IT) R16 2-2
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R16-3-1.pdf">
+        BTech(IT) R16 3-1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-IT-R16-3-2.pdf">
+        BTech(IT) R16 3-2
+      </Button>
     </div>
   );
 };
