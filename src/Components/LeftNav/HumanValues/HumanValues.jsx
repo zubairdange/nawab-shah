52791394d1bbe335dd849b6fcea216fe0d27364c
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-11 12:58:39 +0530

    added content in human values component

--- src/Components/LeftNav/HumanValues/HumanValues.jsx
+++ src/Components/LeftNav/HumanValues/HumanValues.jsx
@@ -1,8 +1,159 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 export const HumanValues = () => {
   return (
     <div>
-      <h1>Human Values</h1>
+      <h3 className="headerclass">Human Values and Professional Ethics</h3>
+      <p>
+        The Environment study is the part of the curriculum for the B.Tech first
+        &amp; Second year students. The subject faculties make the students
+        aware about the basic components of the Environment. The institution
+        organizes the Blood Donation Camps for the awareness of importance of
+        hygiene in the students.
+      </p>
+      {/* <Table>
+        <tbody></tbody>
+        <thead>
+          <tr>
+            <th id="Th22">S.NO</th>
+            <th id="Th23">Critical Issue</th>
+            <th id="Th24">
+              Code &amp; Title of the subject wherein the issue is addressed
+            </th>
+            <th id="Th25">Unit Number</th>
+            <th id="Th26">Remarks</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <h3>
+                <a>
+                  <b>1</b>
+                </a>
+              </h3>
+            </td>
+            <td>
+              <h3>
+                <a>
+                  <b>Human Values and Professional Ethics</b>
+                </a>
+              </h3>
+            </td>
+            <td>
+              <h3>A50018: Human values and professional ethics</h3>
+            </td>
+            <td>
+              <p>
+                Unit I: Natural acceptance. Unit II : Understanding self
+                Harmony.
+                <br />
+                Unit III: Understanding harmony in society.
+                <br />
+                Unit IV: Understanding Nature &amp; Existence harmony.
+                <br />
+                Unit V: Human values acceptance.
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </Table> */}
+      <table class="category table table-striped table-bordered table-hover">
+        <tbody></tbody>
+        <thead>
+          <tr>
+            <th id="Th22">S.NO</th>
+            <th id="Th23">Critical Issue</th>
+            <th id="Th24">
+              Code &amp; Title of the subject wherein the issue is addressed
+            </th>
+            <th id="Th25">Unit Number</th>
+            <th id="Th26">Remarks</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="cat-list-row0">
+            <td headers="categorylist_header_title" class="list-title">
+              <h6>
+                <a>
+                  <b>1</b>
+                </a>
+              </h6>
+            </td>
+            <td headers="categorylist_header_author" class="list-author">
+              <h6>
+                <a>
+                  <b>Human Values and Professional Ethics</b>
+                </a>
+              </h6>
+            </td>
+            <td headers="categorylist_header_author" class="list-author">
+              <h6>A50018: Human values and professional ethics</h6>
+            </td>
+            <td headers="categorylist_header_author" class="list-author">
+              <p>
+                Unit I: Natural acceptance.
+                <br />
+                Unit II : Understanding self Harmony.
+                <br />
+                Unit III: Understanding harmony in society.
+                <br />
+                Unit IV: Understanding Nature &amp; Existence harmony.
+                <br />
+                Unit V: Human values acceptance.
+              </p>
+            </td>
+            <td headers="categorylist_header_author" class="list-author">
+              <p>
+                To develop the essential complementarily between 'Values' and
+                'Skills' to ensure sustained happiness and prosperity which are
+                the core aspirations of all human beings.
+              </p>
+            </td>
+          </tr>
+          <tr class="cat-list-row0">
+            <td headers="categorylist_header_title" class="list-title">
+              <h6>
+                <a>
+                  <b>2</b>
+                </a>
+              </h6>
+            </td>
+            <td headers="categorylist_header_author" class="list-author">
+              <h6>
+                <a>
+                  <b>Professional Ethics</b>
+                </a>
+              </h6>
+            </td>
+            <td headers="categorylist_header_author" class="list-author">
+              <h6>MC500HS: Professional Ethics</h6>
+            </td>
+            <td headers="categorylist_header_author" class="list-author">
+              <p>
+                UNIT – I: Introduction to Professional Ethics.
+                <br />
+                UNIT - II Basic Theories.
+                <br />
+                UNIT - III Professional Practices in Engineering.
+                <br />
+                UNIT – IV Work Place Rights &amp; Responsibilities.
+                <br />
+                UNIT - V Global issues in Professional Ethics.
+                <br />
+              </p>
+            </td>
+            <td headers="categorylist_header_author" class="list-author">
+              <p>
+                To internalize the Values and Ethical Behavior in the personal
+                and Professional careers of students. The students will learn
+                the rights and responsibilities as an employee, team member and
+                a global citizen.
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
