832c1431966b12f94a886a8f11c600ea55fe5480
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-11 17:34:56 +0530

    changes in css

--- src/Components/LeftNav/UGCourses/BE(CSE)/SourceOutcome.jsx
+++ src/Components/LeftNav/UGCourses/BE(CSE)/SourceOutcome.jsx
@@ -6,13 +6,25 @@ export const SourceOutcome = () => {
     <div>
       <h3 className="headerclass">CSE Course Outcomes</h3>
       <h5 className="h5class">CSE-R16</h5>
-      <Button className="buttonclass">BTech(CSE) R16 1-1</Button>
-      <Button className="buttonclass">BTech(CSE) R16 1-2</Button>
+      <Button className="buttonclass" href="MYPDF/BT-CSE-R16-1-1.pdf">
+        BTech(CSE) R16 1-1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-CSE-R16-1-2.pdf">
+        BTech(CSE) R16 1-2
+      </Button>
 
-      <Button className="buttonclass">BTech(CSE) R16 2-1</Button>
-      <Button className="buttonclass">BTech(CSE) R16 2-2</Button>
-      <Button className="buttonclass">BTech(CSE) R16 3-1</Button>
-      <Button className="buttonclass">BTech(CSE) R16 3-2</Button>
+      <Button className="buttonclass" href="MYPDF/BT-CSE-R16-2-1.pdf">
+        BTech(CSE) R16 2-1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-CSE-R16-2-2.pdf">
+        BTech(CSE) R16 2-2
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-CSE-R16-3-1.pdf">
+        BTech(CSE) R16 3-1
+      </Button>
+      <Button className="buttonclass" href="MYPDF/BT-CSE-R16-3-2.pdf">
+        BTech(CSE) R16 3-2
+      </Button>
     </div>
   );
 };
